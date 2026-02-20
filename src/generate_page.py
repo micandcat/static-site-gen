a29f8765d04bb08cd3b62a47f0fa095a3e3d74b9
micandcat <mikeacattell@gmail.com>
2026-02-20 16:00:10 -0500

    added basepath for deployment to GitHub pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,8 @@ import pathlib
 from md_to_html import markdown_to_html_node
 from extract_title import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r", encoding="UTF-8") as f:
@@ -17,7 +18,9 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_data)
 
     replaced = template_data.replace("{{ Title }}", title)
-    final = replaced.replace("{{ Content }}", html)
+    content = replaced.replace("{{ Content }}", html)
+    href = content.replace('href="/', f'href="{basepath}')
+    final = href.replace('src="/', f'src="{basepath}')
 
     dir_path = os.path.dirname(dest_path)
 
@@ -27,7 +30,7 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w", encoding="UTF-8") as f:
         f.write(final)
         
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     
     dir_list = os.listdir(dir_path_content)
 
@@ -35,7 +38,7 @@ def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
         item_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(item_path):
-            generate_page(item_path, template_path, pathlib.Path(f"{dest_path[:-3]}.html"))
+            generate_page(item_path, template_path, pathlib.Path(f"{dest_path[:-3]}.html"), basepath)
         else:
-            generate_page_recursive(item_path, template_path, dest_path)
+            generate_page_recursive(item_path, template_path, dest_path, basepath)
     
--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 import os
+import sys
 import shutil
 from copy_dir import copy_dir
 from generate_page import generate_page_recursive
 
+
+
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
 
     src = "static"
-    dest = "public"
+    dest = "docs"
     shutil.rmtree(dest)
     os.mkdir(dest)
     copy_dir(src, dest)
 
-    generate_page_recursive("content", "template.html", "public")
+    generate_page_recursive("content", "template.html", "docs", basepath)
+
+ 
 
 main()
